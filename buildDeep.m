1f6aa7e8b89621ad6ece7d84ad0fdce4f413567d
Mayank Home Workstation <mayank@ws.home>
2015-09-16 14:10:58 +0530

    Creating a script to be able to compile on cluster itself

---
+++ buildDeep.m
@@ -0,0 +1,16 @@
+mcc -m computeFeaturesCompiled ...
+  -a filehandling/ ...
+  -a misc/ ...
+  -a toolbox/ ...
+  -a optflow_deqing/ ...
+  -a deepmatching/deepmex.mexa64
+if exist('readme.txt','file'),
+  delete('readme.txt');
+end
+if exist('mccExcludedFiles.log','file'),
+  delete('mccExcludedFiles.log');
+end
+if exist('requiredMCRProducts.txt','file'),
+  delete('requiredMCRProducts.txt');
+end
+
